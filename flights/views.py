592c40875caed5d2d22186a5953f6f9111b48200
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-14 11:55:01 +0300

    completed

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,16 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializer,BookingSerializer
+from datetime import date
+
+
+
+class FlightList(ListAPIView):
+    queryset=Flight.objects.all()
+    serializer_class=FlightSerializer
+
+
+
+class BookingList(ListAPIView):
+    queryset=Booking.objects.filter(date__gte=date.today())
+    serializer_class=BookingSerializer
